978a2bb3cd5c7481c25ddae680bac6986ae893f1
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-07-24 20:02:41 +0100

    update

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 # Object Oriented
+#class Object
+#self
+# constructor __init__
+# Encapsulation 
+# dir method
+# add Attribute
+# delete attribute
+# delete Object 
+# class attribute
+# getter setter
+# Access modifiers
+# Inhertance
+# method overrride...
+
 
 class Student:
     # nom age email uns....  attribute 
-    def talk():   # Method Action function
+     
+    def __init__(self,nom,email,age):
+        self.nom = 
+    def talk(self):   # Method Action function
         print(" my name is ?")
         
-    def write(name):
+    def write(self,name):
         print(" my Name is ", name)
     
         
@@ -13,5 +30,11 @@ class Student:
      
 std1 = Student()
 
-std1.talk()
+std1.write("Sara")
+
+std2 = Student()
+std2.write("Ahmed")
+
+#print(std1)
+#print(std2)
 
