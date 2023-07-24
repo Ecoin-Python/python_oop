32f97bd8a2061ef21df13978bc198b25423ade92
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-07-24 19:37:37 +0100

    first comit

---
+++ app.py
@@ -0,0 +1,17 @@
+# Object Oriented
+
+class Student:
+    # nom age email uns....  attribute 
+    def talk():   # Method Action function
+        print(" my name is ?")
+        
+    def write(name):
+        print(" my Name is ", name)
+    
+        
+
+     
+std1 = Student()
+
+std1.talk()
+
