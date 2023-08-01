96157fc5c7ab6384993a40c54c70be284c064373
Abdelouahab Djoudi <djoudi@AppsBunches.com>
2023-08-01 10:18:55 +0100

    add getter and setter

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 # Object Oriented
-# class Object
-# self
-# constructor __init__
+# +class Object
+# +self
+# +constructor __init__
 # Encapsulation 
 # dir method
 # add Attribute
 # delete attribute
 # delete Object 
 # class attribute
-# getter setter
-# Access modifiers
+# +getter setter
+# +Access modifiers
 # Inhertance
 # method overrride...
 # polymo
@@ -17,35 +17,60 @@
 
 class Student:
     # nom age email uns....  attribute 
-    nom = None
-    age = None
-    email = None
+    __nom = None # public 
+    __age = None   # protected
+    __email = None  # private
      # constructor __init__
-    def __init__(self,nom,age,email="a@a.com"):
+    """ def __init__(self,nom,age,email="a@a.com"):
         self.nom = nom
         self.age = age
-        self.email = email
-        
+        self.email = email """
+        #setter
+    def set_nom(self,v):
+        self.nom = v
+    def get_nom(self):
+        return self.nom
+    def set_val(self,value)  :
+        if(type(value)==int):
+            self.value = value 
+        else: 
+            print("the value is not Integer")    
+        #getter
+    def get_val(self) :
+        return self.value
     def talk(self):   # Method Action function
         print(" my name is ?")
         
-    def write(self,name):
-        print(" my Name is ", name)
+    def write(self):
+        print(" my Name is ", self._age)
     
      
-std1 = Student("Ahmed",25,"ahmed@gmail.com")
+#std1 = Student("Ahmed",25,"ahmed@gmail.com")
 
 
-std2 = Student("Sara",15,"sara@gmail.com")
+#std2 = Student("Sara",15,"sara@gmail.com")
 
 
-std3 = Student("ali",13)
-std3.email = "Ali@gmail.com"
+#std3 = Student("ali",13)
 
-std1.write(std1.nom)
-std1.write(std2.nom)
-std3.write(std3.email)
 
 ##################Encapsulation######################
+# std4 = Student()
+# std4.nom = "Imen"
+
+# std4._age = 25
+
+# std4.__email = "imen@gmail.com"
+# print(std4.nom)
+# print(std4._age)
+# print(std4.__email)
+
+std5 = Student()
+std5.set_val(25)
+std5.set_nom("Sara")
+print(std5.get_nom())
+
+print(std5.get_val())
+
 
 
